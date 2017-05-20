19b6227c553e16753a3bae8cc31509195a629dff
Krasder <john.littles.sc@gmail.com>
2017-05-20 11:38:06 +0200

    Add files via upload

---
+++ Piccoli.Giovanni.5H.GStation/Piccoli.Giovanni.5H.GStation/Calendario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piccoli.Giovanni._5H.GStation
+{
+    class Calendario
+    {
+        public string Evento { get; set; }
+        public string Data { get; set; }
+    }
+}
